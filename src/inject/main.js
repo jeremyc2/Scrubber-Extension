34cd006dd82be63e3f1dd317ef418bb1d5a81361
Jeremy Chandler <j0c0erv@HOMEOFFICE.wal-mart.com>
2021-01-21 10:06:54 -0600

    No commit message

--- src/inject/main.js
+++ src/inject/main.js
@@ -79,7 +79,7 @@ function loadFrames(video) {
         thumbnails.forEach(thumbnail => thumbnail.parentNode.removeChild(thumbnail));
 
         if(video.src == "") {
-            console.log("Video source derived from href");
+            console.log("Video source derived from href: " + window.location.href);
             tempVideo.src = window.location.href;
         } else {
             tempVideo.src = video.src;
@@ -131,7 +131,7 @@ function loadFrames(video) {
     console.log("Cross Origin: ", video.crossOrigin);
 
     if(video.src == "") {
-        console.log("Video source derived from href");
+        console.log("Video source derived from href: " + window.location.href);
         tempVideo.src = window.location.href;
     } else {
         tempVideo.src = video.src;
